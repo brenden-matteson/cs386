ff6fd46fb516bf7dbd22e343df589073712025f7
Brenden Matteson <brendennavigator@gmail.com>
2025-04-09 00:29:03 -0700

    updated total pay test to work with new class structure of income.

--- node-test/totalPay.test.js
+++ node-test/totalPay.test.js
@@ -1,5 +1,19 @@
-const expenseTracker = require('../SimpleCents/expense-tracker/expenseTracker.js');
+const { hourly, salary, contract } = require('../SimpleCents/expense-tracker/expenseTracker.js');
 
-test('multiplies 17.40 per hour for 20 hours to be 18,096', () => {
-    expect(expenseTracker.totalPay(20, 17.40)).toBe(18096);
-})
+test("hourly worker", async () => {
+    const obj = new hourly(17.40, 20);
+    const result = obj.totalPay();
+    expect(result).toBe(18096);
+});
+
+test("salary worker", async () => {
+    const obj = new salary(25000);
+    const result = obj.totalPay();
+    expect(result).toBe(25000);
+});
+
+test("contract worker", async () => {
+    const obj = new contract([100, 100, 1000, 1000]);
+    const result = obj.totalPay();
+    expect(result).toBe(2200);
+});
